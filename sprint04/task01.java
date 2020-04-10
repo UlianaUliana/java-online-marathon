42a6be55bef96c4c1355ec78061bc957359f8b1a
UlianaUliana <43820988+UlianaUliana@users.noreply.github.com>
2020-04-10 12:59:18 +0300

    Create task01.java

---
+++ sprint04/task01.java
@@ -0,0 +1,7 @@
+//Create a Pet class with public fields name of type String and age of type int that are not initialized.
+// For correct passing of all tests don't use print and println methods in your code.
+
+class Pet {
+  public String name;
+  public int age;
+}
