d7afbb22b1e3cb6a1a3e1d4e795cde630d14a2e5
UlianaUliana <43820988+UlianaUliana@users.noreply.github.com>
2020-05-05 14:29:04 +0300

    Create task03.java

---
+++ sprint11/task03.java
@@ -0,0 +1,43 @@
+//Suppose, we class Plant from the task2, which includes private fields String name,
+// Color color and Type type, and constructor with three String parameters where these fields
+// are initialized. Color and Type are Enum.
+//Color contains White, Red, Blue entries.
+//Type contains Rare and Available entries.
+//
+//And we have classes ColorException and TypeException as derived from Exception.
+//
+//The constructor of Plant throws a corresponding exception whenever an inappropriate
+// parameter is passed.
+//(public Plant(String type, String color, String name) throws ColorException, TypeException)
+//Write a static tryCreatePlant method that takes 3 string parameters - type, color and name
+// and returns an instance of Plant, created based on passed params. (
+// Don't create any classes, write as if you are already inside a class.)
+//The tryCreatePlant method should catch exceptions that can be thrown.
+// If inappropriate type passed as a parameter, a Plant object should be returned anyway,
+// with Ordinary type. If an inappropriate color is passed, set Red color for created instance.
+
+
+static Plant tryCreatePlant(String type, String color, String name){
+    try {
+        return new Plant(type,color,name);
+    } catch (TypeException te) {
+        type = "Ordinary";
+        try {
+            return new Plant(type,color,name);
+        } catch (ColorException | TypeException cte) {
+            color = "Red";
+        }
+    } catch (ColorException ce) {
+        color = "Red";
+        try {
+            return new Plant(type,color,name);
+        } catch (ColorException | TypeException cte) {
+            type = "Ordinary";
+        }
+    }
+    try{
+        return new Plant(type,color,name);
+    } catch (TypeException | ColorException e) {
+        throw new RuntimeException();
+    }
+}
